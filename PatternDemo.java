2f972138293ea319ac1cdc29d0dc9fd47190f7e5
2451-23-751-021 <245123751021@mvsrec.edu.in>
2026-03-23 15:08:39 +0530

    PatternDemo

--- PatternDemo.java
+++ PatternDemo.java
@@ -2,7 +2,7 @@ public class PatternDemo{
   public static void main(String[] args){
     int n=8;
     for(int i=1;i<=n;i++){
-      for(j=1;j<=n;j++){
+      for(int j=1;j<=n;j++){
         System.out.println("*");
       }
       System.out.println();
