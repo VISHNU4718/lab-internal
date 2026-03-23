2dce423b956a4f29932e28fb2b6e29516929fbb9
2451-23-751-021 <245123751021@mvsrec.edu.in>
2026-03-23 15:10:06 +0530

    Fix nested loop to print pattern correctly

--- PatternDemo.java
+++ PatternDemo.java
@@ -2,7 +2,7 @@ public class PatternDemo{
   public static void main(String[] args){
     int n=8;
     for(int i=1;i<=n;i++){
-      for(int j=1;j<=n;j++){
+      for(int j=1;j<=i;j++){
         System.out.println("*");
       }
       System.out.println();
