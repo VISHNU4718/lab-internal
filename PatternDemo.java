8703269fad883513bf0c4046a0dd257f153754ee
2451-23-751-021 <245123751021@mvsrec.edu.in>
2026-03-23 15:14:31 +0530

    Change output format to include newline after asterisk

--- PatternDemo.java
+++ PatternDemo.java
@@ -3,7 +3,7 @@ public class PatternDemo{
     int n=8;
     for(int i=1;i<=n;i++){
       for(int j=1;j<=i;j++){
-        System.out.println("*");
+        System.out.println("*\n");
       }
       System.out.println();
     }
