c34bebc59c0ac10ca2912b0a41608e785af0ee16
2451-23-751-021 <245123751021@mvsrec.edu.in>
2026-03-23 15:02:53 +0530

    Refactor Add class to PatternDemo with asterisk pattern
    Updated the class from Add to PatternDemo and added a main method to print a pattern of asterisks.

---
+++ PatternDemo.java
@@ -0,0 +1,13 @@
+public class PatternDemo{
+  public static void main(String[] args){
+    int n=8;
+    for(int i=1;i<=n;i++){
+      for(j=1;j<=n;j++){
+        System.out.println("*");
+      }
+      System.out.println();
+    }
+  }
+}
+        
+    
